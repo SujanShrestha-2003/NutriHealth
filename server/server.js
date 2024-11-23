53d07d8520fe7665f06fa012642ccfa7a679decd
SujanShrestha-2003 <s2003shrestha@gmail.com>
2024-11-23 18:35:10 +0545

    connect to db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to Mongoose');
+}).catch((err) => {
+    console.log(err);
+})
 app.listen(5000,() => {
     console.log('Server listening on port 5000');
 });
