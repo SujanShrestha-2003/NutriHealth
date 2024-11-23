517df43770b7b62bbcca898081b9174e8cce90be
SujanShrestha-2003 <s2003shrestha@gmail.com>
2024-11-23 16:36:12 +0545

    add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx';
+import StartingPage from './pages/StartingPage.jsx';
+import Signup from './pages/Signup.jsx';
+import Login from './pages/Login.jsx';
+import Profile from './pages/Profile.jsx';
+import Questioneer from './pages/Questioneer.jsx';
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  return <BrowserRouter>
+
+  <Routes>
+    <Route path='/' element={<StartingPage />}></Route>
+    <Route path='/landing' element={<LandingPage />}></Route>
+    <Route path='/signup' element={<Signup />}></Route>
+    <Route path='/login' element={<Login />}></Route>
+    <Route path='/profile' element={<Profile />}></Route>
+    <Route path='/questioneer' element={<Questioneer />}></Route>
+  </Routes>
+
+  </BrowserRouter>
 };
 
 export default App;
---
+++ client/src/pages/Questioneer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Questioneer = () => {
+  return (
+    <div>Questioneer</div>
+  )
+}
+
+export default Questioneer
